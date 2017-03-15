a0a2f38380cc4efbb71a50ac2a3baab817f1eff9
Alfred <shenyuanchen@vchangyi.com>
2017-03-15 23:37:19 +0800

    server

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,6 @@
 // nodejs 中的path模块
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
@@ -13,7 +14,7 @@ module.exports = {
     output: {
         // 输出路径是 myProject/output/static
         path: path.resolve(__dirname, '../output/static'),
-        publicPath: 'static/',
+        publicPath: '/',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[chunkhash].js'
     },
@@ -36,5 +37,13 @@ module.exports = {
                 loader: 'style-loader!css-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My Webpack',
+            filename: 'index.html',
+            template: path.resolve(__dirname, '../app/index/index.html'),
+            inject: true
+        })
+    ]
 }
