17d6fc243682752e401c9948d6f8e83efb863cae
shenyuancheng <shenyuanchen@mail.xusenintl.com>
2017-03-15 19:09:51 +0800

    s

---
+++ build/webpack.config.js
@@ -0,0 +1,40 @@
+// nodejs 中的path模块
+var path = require('path');
+
+module.exports = {
+    // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
+    entry: {
+        index: path.resolve(__dirname, '../app/index/index.js'),
+        vendors: [
+            'Vue'
+        ]
+    },
+    // 输出配置
+    output: {
+        // 输出路径是 myProject/output/static
+        path: path.resolve(__dirname, '../output/static'),
+        publicPath: 'static/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[id].[chunkhash].js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.vue']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/, 
+                loader: 'vue'   
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel?presets=es2015',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+}
